29641f89b9733d3992296e241208212c59aa2563
Gccool <97748617+Gccool@users.noreply.github.com>
2024-09-16 22:02:18 +0100

    Update computerSide.py

--- python/computerSide.py
+++ python/computerSide.py
@@ -75,8 +75,28 @@ async def main():
     else:
         return jsonify({'error': 'No media information available'}), 404
             
-            
-            
+@app.route('/skip')            
+async def skip():
+	
+    sessions = await mediaManager.request_async()
+    
+    
+    current_session = sessions.get_sessions()
+
+    for ses in current_session:
+        if ses.source_app_user_model_id == "AppleInc.AppleMusicWin_nzyj5cx40ttqa!App":
+            current_session = ses
+            break
+
+    
+
+    #breakpoint()
+
+    if current_session:
+        if current_session.source_app_user_model_id == "AppleInc.AppleMusicWin_nzyj5cx40ttqa!App":
+            #if current_session.IsNextEnabled == True:
+            current_session.                
+                               
             
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
