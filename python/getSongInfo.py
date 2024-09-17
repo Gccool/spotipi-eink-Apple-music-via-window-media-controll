ed431722374cadc85d2449b6404b713ecc315574
gccool <georgejclayton3@icloud.com>
2024-09-17 17:17:19 +0100

    update get thumbnail

--- python/getSongInfo.py
+++ python/getSongInfo.py
@@ -32,10 +32,29 @@ def get_access_token():
 
 
 
-def get_thumbnail(access_token, track_name, artist_name):
+def get_access_token():
+    auth_str = f"{client_id}:{client_secret}"
+    b64_auth_str = base64.b64encode(auth_str.encode()).decode()
+    headers = {
+        "Authorization": f"Basic {b64_auth_str}",
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    response = requests.post('https://accounts.spotify.com/api/token', headers=headers, data={"grant_type": "client_credentials"})
+    return response.json()['access_token']
+
+# Search for the track
+def get_thumbnail(access_token, track_name, artist_name, album_name):
     headers = {
         "Authorization": f"Bearer {access_token}"
     }
+
+    query = f"track:{track_name} artist:{artist_name} album:{album_name}"
+    response = requests.get(f"https://api.spotify.com/v1/search?q={query}&type=track", headers=headers)
+    tracks = response.json()['tracks']['items']
+    if tracks:
+        return tracks[0]['album']['images'][0]['url']
+
+
     query = f"track:{track_name} artist:{artist_name}"
     response = requests.get(f"https://api.spotify.com/v1/search?q={query}&type=track", headers=headers)
     tracks = response.json()['tracks']['items']
